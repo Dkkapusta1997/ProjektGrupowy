8e0ff4182b11a781fb04935dd408eefa6fbb81cb
unknown <Dkkapusta1997@yahoo.pl>
2020-09-09 00:21:36 +0200

    cos tam

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express()
 const port=process.env.PORT || 3000
 
 app.get('/',(req,res)=>{
-    res.send('Witaj znowu w programowaniu123')
+    res.send('Witaj znowu w programow11aniu123')
 })
 
 app.listen(port,()=>{
