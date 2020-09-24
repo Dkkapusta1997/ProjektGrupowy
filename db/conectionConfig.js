07744edb896cd63fb6db52af8fd5736d78c8d6fc
Kapi <Dkkapusta1997@yahoo.pl>
2020-09-24 12:02:43 +0200

    change login date to db

--- db/conectionConfig.js
+++ db/conectionConfig.js
@@ -2,8 +2,8 @@ module.exports= connectionConfig = {
     client: 'mysql2',
     connection:{
         host: 'freedb.tech',
-        user: 'freedb_PROJ1920',
-        password: 'PROJ1920',
-        database: 'freedb_PROJ1920'
+        user: 'freedbtech_PROJ_1920',
+        password: 'PROJ_1920',
+        database: 'freedbtech_PROJ_1920'
     }
 };
